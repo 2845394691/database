e116906181133b6a1d5f1d3cd9c4838290b81ccc
PoisonedDog <3233602143@qq.com>
2023-05-28 17:09:48 +0800

    管理员部门展示

--- myapp/views/admin_depart.py
+++ myapp/views/admin_depart.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from myapp.models import Department
 from myapp.models import Departmentposition
+from myapp import models
+
 
 def detail(request):
-    if request.method=="GET":
-        return render(request,"admin_depart.html")
-
-
+    """部门管理"""
+    departs = models.Department.objects.all()
+    total_count_depart = departs.count()
+    total_count_staff = 0
+    depart_names = []
+    depart_counts = []
+    for depart in departs:
+        staff_count = models.Staff.objects.filter(departmentno=depart.departmentno).count()
+        depart_names.append(depart.departmentname)
+        depart_counts.append(staff_count)
+        total_count_staff = total_count_staff + staff_count
+    if request.method == "GET":
+        return render(request, "admin_depart.html",
+                      {'names': depart_names, 'counts': depart_counts, 'depart_count': total_count_depart,
+                       'staff_count':total_count_staff})
