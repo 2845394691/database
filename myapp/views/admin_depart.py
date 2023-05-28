e70ea4ab6083ee0cb7fffe812e9e2fc09de3d0cb
PoisonedDog <3233602143@qq.com>
2023-05-28 18:45:59 +0800

    管理端部门管理

--- myapp/views/admin_depart.py
+++ myapp/views/admin_depart.py
@@ -2,7 +2,20 @@ from django.shortcuts import render, redirect
 from myapp.models import Department
 from myapp.models import Departmentposition
 from myapp import models
+import time
+import random
 
+def get_rander_num():
+    while True:
+        # 获取系统当前时间作为种子
+        seed = int(time.time())
+        # 创建Random对象
+        r = random.Random(seed)
+        # 生成1000到9999之间的随机数
+        result = r.randint(1, 9999)
+        object = models.Department.objects.filter(departmentno=result)
+        if not object:
+            return result
 
 def detail(request):
     """部门管理"""
@@ -20,3 +33,6 @@ def detail(request):
         return render(request, "admin_depart.html",
                       {'names': depart_names, 'counts': depart_counts, 'depart_count': total_count_depart,
                        'staff_count':total_count_staff})
+    name = request.POST.get('name')
+    models.Department.objects.create(departmentno=get_rander_num(), departmentname=name)
+    return redirect("/admin/depart/")
--- myapp/views/admin_info.py
+++ myapp/views/admin_info.py
@@ -1,24 +1,29 @@
-from django.shortcuts import render,redirect
-from myapp.models import Staff,Manager
+from django.shortcuts import render, redirect
+from myapp.models import Staff, Manager,Department
+
 
 def manager(request):
-    manager=Manager.objects.all()
-    return render(request,'admin_info_manager.html',{"manager":manager})
+    # manager = Manager.objects.all()
+    # return render(request, 'admin_info_manager.html', {"manager": manager})
+    departs = Department.objects.all()
+    return render(request, 'admin_info_manager.html', {"departs": departs})
+
+
 
 def manager_del(request):
-    managerno=request.GET.get('nid')
-    Staff.objects.filter(staffno=managerno).delete()
+    # managerno = request.GET.get('nid')
+    # Staff.objects.filter(staffno=managerno).delete()
+    departno = request.GET.get('nid')
+    Department.objects.filter(departmentno=departno).delete()
     return redirect("/admin/info/manager/")
 
+
 def staff(request):
-    staff=Staff.objects.filter(man_staffno__isnull=False)
-    return render(request,'admin_info_staff.html',{"staff":staff})
+    staff = Staff.objects.filter(man_staffno__isnull=False)
+    return render(request, 'admin_info_staff.html', {"staff": staff})
+
 
 def staff_del(request):
-    staffno=request.GET.get('nid')
+    staffno = request.GET.get('nid')
     Staff.objects.filter(staffno=staffno).delete()
     return redirect("/admin/info/staff/")
-
-
-
-
